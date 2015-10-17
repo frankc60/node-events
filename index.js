22698ff398b53872644f5bf1657153ceca306570
frankc60 <frankc60@gmail.com>
2015-10-17 19:03:11 +0100

    working

---
+++ index.js
@@ -0,0 +1,58 @@
+var events = require("events");
+//*****************************************
+function Publisher_NewsPaper(name){
+	this.emiter = new events.EventEmitter();
+	this.name=name;
+	this.money=0;
+	this.info=function(){
+		console.log("I am a NewsPaper publisher called " + this.name);
+		//need to emit (publish) an event has happened
+		(this.emiter).emit("info",this.name);
+	}
+	this.pressRelease=function(words){
+		console.log("Press Release from the publisher called " + this.name + " ´"+words+"´");
+		(this.emiter).emit("pressRelease",this);
+	}
+	this.addMoney=function(amount){
+		this.money+=amount;
+		(this.emiter).emit("addMoney",this);
+	}
+
+	//add a sub to this object when constructor is called
+	Subscriber(this);
+}
+//*****************************************
+function Subscriber(publisher){
+	
+	//add a sub listener (.on) to the pub eventEmiter
+	(publisher.emiter).on("info",function(who){
+		console.log("subcriber called to publisher: ·" + who + "·");
+	});
+	(publisher.emiter).on("pressRelease",function(who){
+		console.log("pressRelease -subcriber called to publisher: ·" + who.name + "·");
+	});
+	publisher.emiter.on("addMoney",function(who){
+		console.log("Money update for " + who.name + ", new total is " + who.money);
+	});
+
+}
+//*****************************************
+
+var paper1 = new Publisher_NewsPaper("The Press"); 
+var paper2 = new Publisher_NewsPaper("The Outlook"); 
+var paper3 = new Publisher_NewsPaper("The NewEarth"); 
+
+
+
+
+paper1.info();
+paper1.addMoney(125);
+console.log("------------");
+paper2.info();
+console.log("------------");
+paper3.info();
+console.log("------------");
+paper2.pressRelease("the end is nigh");
+paper1.addMoney(125);
+paper1.addMoney(125);
+console.log("console.log out object property money: " +paper1.money);
